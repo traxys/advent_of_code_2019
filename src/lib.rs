c6940dddcda9b363761da70a24e493156cd8fa99
Quentin Boyer <qbsecond@gmail.com>
2019-12-01 14:02:38 +0100

    cargo-aco

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#[macro_use] extern crate aoc_runner_derive;
+
+fn fuel_for_mass(mass: i64) -> i64 {
+    (mass / 3) - 2
+}
+
+fn total_fuel(mass: i64) -> i64 {
+    let mut current_mass = mass;
+    let mut total_fuel_needed = 0;
+    while fuel_for_mass(current_mass) > 0 {
+        total_fuel_needed += fuel_for_mass(current_mass);
+        current_mass = fuel_for_mass(current_mass);
+    }
+    total_fuel_needed
+}
+
+#[aoc(day1, part1)]
+pub fn fuel_of_module(input: &str) -> i64 {
+    input.lines().map(|mass| fuel_for_mass(mass.parse().unwrap())).sum()
+}
+#[aoc(day1, part2)]
+pub fn fuel_of_fuels(input: &str) -> i64 {
+    input.lines().map(|mass| total_fuel(mass.parse().unwrap())).sum()
+}
+
+aoc_lib!{ year = 2019 }
