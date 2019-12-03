5a28dd8acb9a176b5fb46dc3f3ef2541d6d5d1eb
Quentin Boyer <qbsecond@gmail.com>
2019-12-03 15:09:38 +0100

    day3 is done, not so good

--- src/lib.rs
+++ src/lib.rs
@@ -32,4 +32,193 @@ pub fn fuel_of_fuels(input: &str) -> i64 {
         .sum()
 }
 
+#[aoc_generator(day2)]
+pub fn get_intcode(input: &str) -> Vec<u64> {
+    input.split(",").map(|u| u.parse().unwrap()).collect()
+}
+
+fn run_intcode(code: &mut [u64]) -> Result<u64, String> {
+    let mut position = 0;
+    while position < code.len() {
+        match code[position] {
+            1 => {
+                let source_a = code[position + 1] as usize;
+                let source_b = code[position + 2] as usize;
+                let dest = code[position + 3] as usize;
+                code[dest] = code[source_a] + code[source_b];
+            }
+            2 => {
+                let source_a = code[position + 1] as usize;
+                let source_b = code[position + 2] as usize;
+                let dest = code[position + 3] as usize;
+                code[dest] = code[source_a] * code[source_b];
+            }
+            99 => return Ok(code[0]),
+            i => return Err(format!("invalid opcode: {}", i)),
+        }
+        position += 4;
+    }
+    return Err("did not land on 99".to_owned());
+}
+
+#[aoc(day2, part1)]
+pub fn execute_intcode(code: &[u64]) -> Result<u64, String> {
+    let mut code = Vec::from(code);
+    code[1] = 12;
+    code[2] = 2;
+    run_intcode(&mut code)
+}
+
+#[aoc(day2, part2)]
+pub fn find_good_code(initial_memory: &[u64]) -> Result<u64, String> {
+    let target_value = 19690720;
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut code = Vec::from(initial_memory);
+            code[1] = noun;
+            code[2] = verb;
+            if run_intcode(&mut code)? == target_value {
+                return Ok(100 * noun + verb);
+            }
+        }
+    }
+    Err("No pair found".to_owned())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Movement {
+    direction: Direction,
+    length: i64,
+}
+type Path = Vec<Movement>;
+
+#[aoc_generator(day3)]
+pub fn get_paths(input: &str) -> (Path, Path) {
+    let mut paths = input.lines().map(|line| {
+        line.split(",")
+            .map(|point| {
+                let length = point[1..].parse().unwrap();
+                let direction = if point.starts_with("R") {
+                    Direction::Right
+                } else if point.starts_with("D") {
+                    Direction::Down
+                } else if point.starts_with("U") {
+                    Direction::Up
+                } else if point.starts_with("L") {
+                    Direction::Left
+                } else {
+                    panic!("Invalid direction: {}", point)
+                };
+                Movement { direction, length }
+            })
+            .collect()
+    });
+    (paths.next().unwrap(), paths.next().unwrap())
+}
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+use std::collections::HashMap;
+
+fn go_to(start: Point, movement: Movement) -> (Point, Box<dyn Iterator<Item = Point>>) {
+    let mut end_point = start;
+    match movement.direction {
+        Direction::Up => {
+            end_point.y += movement.length;
+            (
+                end_point,
+                Box::new((0..=movement.length).map(move |i| Point {
+                    x: start.x,
+                    y: start.y + i,
+                })),
+            )
+        }
+        Direction::Down => {
+            end_point.y -= movement.length;
+            (
+                end_point,
+                Box::new((0..=movement.length).map(move |i| Point {
+                    x: start.x,
+                    y: start.y - i,
+                })),
+            )
+        }
+        Direction::Left => {
+            end_point.x -= movement.length;
+            (
+                end_point,
+                Box::new((0..=movement.length).map(move |i| Point {
+                    x: start.x - i,
+                    y: start.y,
+                })),
+            )
+        }
+        Direction::Right => {
+            end_point.x += movement.length;
+            (
+                end_point,
+                Box::new((0..=movement.length).map(move |i| Point {
+                    x: start.x + i,
+                    y: start.y,
+                })),
+            )
+        }
+    }
+}
+
+use std::collections::HashSet;
+fn run_path(path: &Path) -> (HashSet<Point>, HashMap<Point, i64>) {
+    let mut points = HashSet::new();
+    let mut steps = HashMap::new();
+    let mut current_point = Point { x: 0, y: 0 };
+    steps.insert(current_point, 0);
+    for movement in path {
+        let (new_point, passed_points) = go_to(current_point, *movement);
+        let start_steps = *steps
+            .get(&current_point)
+            .expect(&format!("Start point is unknown: {:?}", current_point));
+        points.extend(passed_points.enumerate().map(|(i, p)| {
+            let point_step = start_steps + i as i64;
+            steps.entry(p).or_insert(point_step);
+            p
+        }));
+        current_point = new_point;
+    }
+    (points, steps)
+}
+
+#[aoc(day3, part1)]
+pub fn find_nearest_intersection((path1, path2): &(Path, Path)) -> i64 {
+    let (mut passed1, _) = run_path(path1);
+    passed1.remove(&Point { x: 0, y: 0 });
+    let (passed2, _) = run_path(path2);
+    passed1
+        .intersection(&passed2)
+        .map(|Point { x, y }| (x + y).abs())
+        .min()
+        .unwrap()
+}
+
+#[aoc(day3, part2)]
+pub fn find_intersection_with_least_steps((path1, path2): &(Path, Path)) -> i64 {
+    let (mut passed1, steps1) = run_path(path1);
+    passed1.remove(&Point { x: 0, y: 0 });
+    let (passed2, steps2) = run_path(path2);
+    passed1
+        .intersection(&passed2)
+        .map(|p| *steps1.get(p).unwrap() + *steps2.get(p).unwrap())
+        .min()
+        .unwrap()
+}
+
 aoc_lib! { year = 2019 }
